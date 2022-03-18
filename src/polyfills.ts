1f846821c4f8e72e75e88c3bdcf3895a29d4f469
ksz-ksz <kamknitt@gmail.com>
2022-03-18 17:57:24 +0100

    1 file modified

--- src/polyfills.ts
+++ src/polyfills.ts
@@ -1,3 +1,4 @@
 import 'zone.js';
 
-document.body.textContent = 'Check out the browser console.';
+document.body.textContent =
+  'Check out the browser console. If you are running this example from Stackblitz, open this page in a new window first.';
