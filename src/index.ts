c05252e3f2ebf737064ed175600665317d0d47c9
ksz-ksz <kamknitt@gmail.com>
2022-03-20 19:21:31 +0100

    1 file modified

--- src/index.ts
+++ src/index.ts
@@ -3,7 +3,7 @@ import { flow } from '@rxjs-insights/console';
 
 document.body.textContent = `
 Check out the browser console.
-If you are running this example from Stackblitz, open this page in a separate window first.
+If you are running this example from StackBlitz, open this page in a separate window first.
 `;
 
 const subscription = interval(100).pipe(take(3)).subscribe();
