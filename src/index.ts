5ed152fe399ec77c13391baf949b114b68e8d2d2
ksz-ksz <kamknitt@gmail.com>
2022-03-22 15:12:07 +0100

    1 file modified

--- src/index.ts
+++ src/index.ts
@@ -1,11 +1,28 @@
-import { interval, take } from 'rxjs';
+import { EMPTY, NEVER, interval, take, of } from 'rxjs';
 import { flow } from '@rxjs-insights/console';
+import { switchMap } from 'rxjs/operators';
 
 document.body.textContent = `
 Check out the browser console.
 If you are running this example from StackBlitz, open this page in a separate window first.
 `;
 
-const subscription = interval(100).pipe(take(3)).subscribe();
+const subscription = interval(100)
+  .pipe(
+    take(3),
+    switchMap((i) => {
+      switch (i) {
+        case 0:
+          return EMPTY;
+        case 1:
+          return of('one');
+        case 2:
+          return of('two', 'two');
+        default:
+          return NEVER;
+      }
+    })
+  )
+  .subscribe();
 
 setTimeout(() => flow(subscription), 1000);
